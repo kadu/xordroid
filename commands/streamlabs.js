5f5dd94ed17af5649a6021ff0ac0e7a7035b4a02
kadu <kadu@kadu.com.br>
2020-10-27 00:06:29 -0300

    Added back to previous screen after fireworks

--- commands/streamlabs.js
+++ commands/streamlabs.js
@@ -5,6 +5,11 @@ exports.default = (client, obs, mqtt) => {
       if (isBot) return;
 
       //changeScenes.change(client, obs, mqtt, "proto");
+      var thing = this;
+      thing.client = client;
+      thing.obs = obs;
+      thing.mqtt = mqtt;
+      thing.currentScene = changeScenes.getCurrentScene();
 
       if(context.username == "streamlabs") {
         var sendedBy = message.substring(24, message.length-1);
@@ -12,18 +17,22 @@ exports.default = (client, obs, mqtt) => {
           client.say(client.channels[0], `Valeu @${sendedBy} pelo follow, vou até soltar uns rojões!`);
           mqtt.publish("wled/158690", "ON");
           mqtt.publish("wled/158690/api", "FX=90&SN=1");
-          //const saveScene = currentScene;
-          //changeScene("webcam");
           changeScenes.change(client, obs, mqtt, "webcam");
           try {
             setTimeout(()=> {
               mqtt.publish("wled/158690", "OFF");
-              //changeScene(saveScene);
-              //changeScenes.change(client, obs, mqtt, saveScene);
+              console.log("DENTRO thing.currentScene = ", thing.currentScene);
+              changeScenes.change(thing.client, thing.obs, thing.mqtt, thing.currentScene);
             },10000);
           } catch (error) {
             console.log("Erro no settimeout", error);
           }
+        }
+        if(message.substring(0,28) == "just raided the channel with") {
+          console.log("DENTRO");
+          mqtt.publish("wled/158690", "ON");
+          mqtt.publish("wled/158690/api", "FX=66&SN=1");
+          client.say(client.channels[0], `Valeu pela RAID, Sejam todos bem vindos, vou até ascender uma fogueira maneira pra gente curtir juntos \o/`);
         }
       }
   });
