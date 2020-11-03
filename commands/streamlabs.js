01d076e6513f6cc5303dab890d94d38e8223690e
kadu <kadu@kadu.com.br>
2020-11-03 12:16:01 -0300

    Debug inside information

--- commands/streamlabs.js
+++ commands/streamlabs.js
@@ -12,19 +12,23 @@ exports.default = (client, obs, mqtt) => {
       thing.currentScene = changeScenes.getCurrentScene();
 
       if(context.username == "streamlabs") {
+        console.log("**** STREAMLABS MESSAGE ****");
+        console.log(message);
+        console.log("****************************");
+
         var sendedBy = message.substring(24, message.length-1);
         if(message.substring(0,23) == "Thank you for following") {
           client.say(client.channels[0], `Valeu @${sendedBy} pelo follow, vou até soltar uns rojões!`);
           mqtt.publish("wled/158690", "ON");
           mqtt.publish("wled/158690/api", "FX=90&SN=1");
-          mqtt.publish("xordroid/weather/message", `Valeu ae @${sendedBy}`);
+          mqtt.publish("xordroid/message", `Valeu ae @${sendedBy}`);
           changeScenes.change(client, obs, mqtt, "webcam");
           try {
             setTimeout(()=> {
               mqtt.publish("wled/158690", "OFF");
               console.log("DENTRO thing.currentScene = ", thing.currentScene);
               changeScenes.change(thing.client, thing.obs, thing.mqtt, thing.currentScene);
-            },10000);
+            },16000);
           } catch (error) {
             console.log("Erro no settimeout", error);
           }
