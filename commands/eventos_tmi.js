bfbf55e6a14a6008c95ca34642b7e74c0bed7ff0
kadu <kadu@kadu.com.br>
2020-11-05 15:54:10 -0300

    Added some events

--- commands/eventos_tmi.js
+++ commands/eventos_tmi.js
@@ -10,6 +10,9 @@ exports.default = (client, obs, mqtt) => {
   function eventsMessage(effect, chat_message, led_message, timeoutTimer) {
     client.say(thing.client.channels[0], chat_message);
     mqtt.publish("wled/158690", "ON");
+    if(effect == 100) {
+      mqtt.publish("wled/158690/col", "#FF0000");
+    }
     mqtt.publish("wled/158690/api", `FX=${effect}&SN=1`);
     mqtt.publish("xordroid/message", led_message);
     setTimeout(()=> {
@@ -19,7 +22,7 @@ exports.default = (client, obs, mqtt) => {
   }
 
   client.on('subscription', (channel, username, method, message, userstate) => {
-    eventsMessage(100,
+    eventsMessage(66,
       `Ae @${username}, valeu muitão pelo SUB, o coração até para!!! Tks`,
       `a Live agradece @${username}`,
       20000);
@@ -27,10 +30,24 @@ exports.default = (client, obs, mqtt) => {
 
   client.on("raided", (channel, username, viewers) => {
     console.log(channel);
-    eventsMessage(100,
-      `Estamos recebendo uma super raid do pessoal da live => @${username}, valeu pela raid e sejam todos bem vindos (${viewers} viewers!) `,
-      `Ei você que veio da live  @${username}, seja bem vindo!`,
+    eventsMessage(57,
+      `Recebendo uma super raid do pessoal da live @${username}, valeu pela raid e sejam todos bem vindos!) `,
+      `Nossa, tem ${viewers} chegando, ta chovendo gente aqui!`,
       20000);
+  });
+
+  client.on("cheer", (channel, userstate, message) => {
+    eventsMessage(40,
+      `Olha lá, recebendo um cheer (eu nem sei direito o que é isso), foram ${userstate.bits}`,
+      `Valeu pelos bits!`);
+  });
+
+  client.on("join", (channel, username, self) => {
+    mqtt.publish("xordroid/message", `Dedoduro: Bem vindo ${username}`);
+  });
+
+  client.on("part", (channel, username, self) => {
+    mqtt.publish("xordroid/message", `Dedoduro: Tchau ${username}`);
   });
 
   // client.on("raw_message", (messageCloned, message) => {
