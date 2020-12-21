8fbc2acc46337f856872dd882468d597f49b5f69
kadu <kadu@kadu.com.br>
2020-12-21 16:20:10 -0300

    Added projects list

--- commands/projetos.js
+++ commands/projetos.js
@@ -7,8 +7,13 @@ exports.default = (client, obs, mqtt, messages) => {
             case '!projetos':
                 client.say(
                     target,
-                    `Veja a lista de projetos: -> !xordroid (eu mesmo, o bot), !streamdeck (hello world dos makers na twitch.tv), !gatekeeper (o porteiro)`,
-                );
+                    `Veja a lista de projetos: ->
+                    !xordroid (eu mesmo, o bot),
+                    !streamdeck (hello world dos makers na twitch.tv),
+                    !gatekeeper (o porteiro)
+                    !pcb_natal (Enfeite de arvore de natal)
+                    `,
+              );
                 break;
             default:
                 break;
