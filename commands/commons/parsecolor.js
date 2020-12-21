68ac97d600b8a09ae8f600014badcd2678ab11c0
kadu <kadu@kadu.com.br>
2020-12-21 16:19:13 -0300

    added parser to color with translation support

---
+++ commands/commons/parsecolor.js
@@ -0,0 +1,42 @@
+const dotenv = require('dotenv');
+
+const namedColors = require("color-name-list");
+const translate = require('translate');
+
+dotenv.config();
+
+translate.engine = process.env.TRANSLATE_ENGINE;
+translate.key = process.env.TRANSLATE_KEY;
+
+const capitalize = (string) => {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+const capitalize2 = (string) => {
+  return string.split(' ').map(capitalize).join(' ')
+  .replace(" In ", " in ")
+  .replace(" Of ", " of ");
+}
+
+const parseColor = async (color) => {
+  let isColor = /^#[0-9A-F]{6}$/i.test(color);
+  if(!isColor) {
+    isColor = /^[0-9A-F]{6}$/i.test(color);
+  }
+
+  if(!isColor) {
+    let cp = capitalize2(color);
+    let someNamedColor = namedColors.find(color => color.name === cp);
+    if(typeof someNamedColor !== 'undefined') {
+      return someNamedColor.hex;
+    } else {
+      const translated_color = await translate(cp, { from: 'pt', to: 'en' });
+      someNamedColor = namedColors.find(color => color.name === translated_color);
+      return someNamedColor.hex;
+    }
+  } else {
+    return color;
+  }
+}
+
+exports.parseColor = parseColor;
