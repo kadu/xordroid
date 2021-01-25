19ad6fada06309a4c7221518228890c8d93be79b
kadu <kadu@kadu.com.br>
2021-01-25 19:59:08 -0300

    Custom rewards

--- commands/channel_points.js
+++ commands/channel_points.js
@@ -1,9 +1,17 @@
 const parseColor = require('./commons/parsecolor');
+const mongoose = require('mongoose');
 
 const CP_RecompensaTeste = '8c6749f7-9c70-477e-9081-5840ee39033e';
 const CP_LuzCenario = 'e830937d-05c7-4fa0-911a-fcb4f5ed272f';
 
-exports.default = (client, obs, mqtt, messages) => {
+
+const botSchema = new mongoose.Schema({
+  userid: String,
+  points: Number
+});
+
+
+exports.default = (client, obs, mqtt, messages, botDB) => {
     client.on("raw_message", async (messageCloned, message) => {
       if(message.tags && message.tags['custom-reward-id']) {
         console.log(`Debug - custom-reward-id = ${message.tags['custom-reward-id']}`);
@@ -12,11 +20,13 @@ exports.default = (client, obs, mqtt, messages) => {
           let sendcolor = await parseColor.parseColor(message.params[1]);
           if(sendcolor !== -1) {
             mqtt.publish("cmnd/lightrgb02/Color2", sendcolor);
+
+            const silence = new botDB({ userid: `${message.tags['username']}`, points: 10 });
+            silence.save();
+
           }
           else {
-
           }
-
         }
 
         if (message.tags['custom-reward-id'] === CP_RecompensaTeste) {
