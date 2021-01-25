f32f80d2a02163be7177b3596197bcfd2745d7bb
kadu <kadu@kadu.com.br>
2021-01-25 19:58:30 -0300

    Mongo test

---
+++ commands/mongotest.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const botSchema = new mongoose.Schema({
+  userid: String,
+  points: Number
+});
+
+
+exports.default = (client, obs, mqtt, messages, botDB) => {
+    client.on('message', async (target, context, message, isBot) => {
+        if (isBot) return;
+
+        switch (message) {
+            case '!mongotest':
+                client.say(
+                    target,
+                    `Valeu por testar o mongoDB!`,
+                );
+
+                const silence = new botDB({ userid: 'To na mongoTest', points: 10 });
+                console.log("******************");
+                console.log(silence.userid); // 'Silence'
+                console.log("******************");
+                silence.save();
+                break;
+
+            case '!getmongo':
+              let x = await botDB.find({"userid" : "Silence 17"}).exec();
+              console.log(x[0]._doc.userid);
+
+              break;
+            default:
+                break;
+        }
+    });
+};
+
