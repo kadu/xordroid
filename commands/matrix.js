721cbd4566f071cb1c0d8b3aa6a831ce574b66b4
kadu <kadu@kadu.com.br>
2021-01-25 19:58:46 -0300

    new Matrix

--- commands/matrix.js
+++ commands/matrix.js
@@ -1,6 +1,10 @@
+const CP_Matrix = '1c880a37-8dc6-487d-97c6-8f05963c5c75';
+
 exports.default = (client, obs, mqtt, messages) => {
   client.on('message', (target, context, message, isBot) => {
     if (isBot) return;
+
+    if(context.username !== 'kaduzius') return;
 
     let parsedMessage = message.split(" ");
     switch (parsedMessage[0]) {
@@ -13,4 +17,12 @@ exports.default = (client, obs, mqtt, messages) => {
         break;
     }
   });
+
+  client.on("raw_message", async (messageCloned, message) => {
+    if(message.tags['custom-reward-id'] === CP_Matrix) {
+      let fullMessage = message.params[1].normalize('NFD').replace(/[\u0300-\u036f]/g, "");
+      messages.push(fullMessage);
+    }
+  });
+
 };
