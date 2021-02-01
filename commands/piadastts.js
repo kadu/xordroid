e5fefee068540bb0597d14fbd91a7fe4b090a2b8
kadu <kadu@kadu.com.br>
2021-02-01 02:49:19 -0300

    Added randon audio ends

--- commands/piadastts.js
+++ commands/piadastts.js
@@ -2,6 +2,22 @@ const bent = require('bent');
 const getJSON = bent('json');
 const jokeAPIURL = "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit";
 const piadaAPIURL = "https://us-central1-kivson.cloudfunctions.net/charada-aleatoria";
+const cartoonAudioURL = "https://actions.google.com/sounds/v1/cartoon/";
+const cartoonAudios = [
+  "slide_whistle_to_drum.ogg",
+  "slide_whistle.ogg",
+  "siren_whistle.ogg",
+  "pop.ogg",
+  "concussive_hit_guitar_boing.ogg",
+  "cartoon_boing.ogg",
+];
+
+function getFunAudio() {
+  const randomElement = cartoonAudios[Math.floor(Math.random() * cartoonAudios.length)];
+  return cartoonAudioURL + randomElement;
+}
+
+
 
 exports.default = (client, obs, mqtt, messages, botDB, commandQueue, ttsQueue) => {
     client.on('message', async (target, context, message, isBot) => {
@@ -23,7 +39,7 @@ exports.default = (client, obs, mqtt, messages, botDB, commandQueue, ttsQueue) =
 
             case '!piada':
               response = await getJSON(piadaAPIURL);
-              msgpiada = `<speak>${response.pergunta}<break time="1400ms"/>${response.resposta}<audio src="https://actions.google.com/sounds/v1/cartoon/cartoon_boing.ogg"/></speak>`;
+              msgpiada = `<speak>${response.pergunta}<break time="1400ms"/>${response.resposta}<audio src="${getFunAudio()}"/></speak>`;
               ttsQueue.push( {'msg': msgpiada, 'lang': 'pt-BR', 'inputType': 'ssml'});
               break;
             default:
