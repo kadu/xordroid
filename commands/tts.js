3145c55733118303a71c9a79e6f59f9bd6e929ba
kadu <kadu@kadu.com.br>
2021-02-01 01:38:18 -0300

    Clean code

--- commands/tts.js
+++ commands/tts.js
@@ -1,36 +1,3 @@
-// var player = require('play-sound')(opts = {});
-// const textToSpeech = require('@google-cloud/text-to-speech');
-// const fs = require('fs');
-// const util = require('util');
-// const client = new textToSpeech.TextToSpeechClient();
-
-// async function quickStart(message) {
-//   // The text to synthesize
-//   const text = message;
-
-//   // Construct the request
-//   const request = {
-//     input: {text: text},
-//     // Select the language and SSML voice gender (optional)
-//     voice: {languageCode: 'pt-BR', ssmlGender: 'NEUTRAL'},
-//     // select the type of audio encoding
-//     audioConfig: {audioEncoding: 'MP3'},
-//   };
-
-//   // Performs the text-to-speech request
-//   const [response] = await client.synthesizeSpeech(request);
-//   // Write the binary audio content to a local file
-//   const writeFile = util.promisify(fs.writeFile);
-//   await writeFile('output.mp3', response.audioContent, 'binary');
-//   setTimeout(() => {
-//     player.play('output.mp3', function(err){
-//       if (err) throw err
-//       return;
-//     });
-//   },1000);
-//   console.log('Audio content written to file: output.mp3');
-// }
-
 exports.default = (client, obs, mqtt, messages, botDB, commandQueue, ttsQueue) => {
     client.on('message', (target, context, message, isBot) => {
         if (isBot) return;
