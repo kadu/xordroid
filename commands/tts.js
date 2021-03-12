354ca303de1ec13359de34dbc4c37c06863f42e8
kadu <kadu@kadu.com.br>
2021-03-12 15:25:16 -0300

    enable tts again

--- commands/tts.js
+++ commands/tts.js
@@ -1,8 +1,12 @@
 exports.default = (client, obs, mqtt, messages, botDB, commandQueue, ttsQueue) => {
     client.on('message', (target, context, message, isBot) => {
         if (isBot) return;
-
         let parsedMessage = message.split(" ");
+
+        // if(parsedMessage[0] === '!tts' || parsedMessage[0] === '!entts' || parsedMessage[0] === '!pttts' || parsedMessage[0] === '!frtts') {
+        //   return;
+        // }
+
         if(parsedMessage[0] === '!tts') {
           let fullMessage = context["display-name"] + " disse: " + message.replace("!tts ","");
           ttsQueue.push( {'msg': fullMessage, 'lang': 'pt-BR','inputType': 'text'});
