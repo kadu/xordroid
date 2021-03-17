890bfd81a07cb0df80b079b547faa188d8bd7f36
kadu <kadu@kadu.com.br>
2021-03-17 19:59:55 -0300

    Added weather and db

--- commands/weather.js
+++ commands/weather.js
@@ -1,18 +1,38 @@
 const dotenv = require('dotenv');
 const bent = require('bent');
 const getJSON = bent('json');
+const sqlite3 = require('sqlite3').verbose();
+const sqlite = require('sqlite');
 const WURL = 'https://api.openweathermap.org/data/2.5/weather?units=metric&lang=pt_br&q=';
+var db = null;
 
+async function createDB() {
+  try {
+    db = await sqlite.open({ filename: './weathermap.db', driver: sqlite3.Database });
+    await db.run(`CREATE TABLE IF NOT EXISTS weathermap ( id INTEGER PRIMARY KEY AUTOINCREMENT, timestamp DATETIME DEFAULT CURRENT_TIMESTAMP, video_time TEXT, username TEXT, city TEXT, lat TEXT, long TEXT, temp TEXT, temp_feelslike TEXT)`);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+async function getStreamTime(obs) {
+  return obs.send('GetStreamingStatus', {})
+    .then( (value) => {
+      return value.streamTimecode;
+    })
+    .catch( err => {
+      return 0;
+    });
+}
+
+createDB();
 
 //api.openweathermap.org/data/2.5/weather?q={city name},{state code},{country code}&appid={API key}
 
 dotenv.config();
 openWeatherKey = process.env.OPENWEATER_KEY;
 
-//let coord = {"latitude": 37, "longitude": -122 };
-
-
-exports.default = (client, obs, mqtt, messages, botDB, commandQueue, ttsQueue) => {
+exports.default = (client, obs, mqtt, messages, commandQueue, ttsQueue) => {
     client.on('message', async (target, context, message, isBot) => {
         if (isBot) return;
 
@@ -24,11 +44,31 @@ exports.default = (client, obs, mqtt, messages, botDB, commandQueue, ttsQueue) =
                 let cidade = fullMessage;
                 let response = "";
                 try {
-                  response = await getJSON(`${WURL}${cidade}&appid=${openWeatherKey}`);
-                  // console.log(response);
+                  response         = await getJSON(`${WURL}${cidade}&appid=${openWeatherKey}`);
+                  const lon        = response.coord.lon;
+                  const lat        = response.coord.lat;
+                  const temp       = response.main.temp;
+                  const feels_like = response.main.feels_like;
+                  const temp_descr = response.weather[0].description;
+                  const city       = response.name;
+                  const country    = response.sys.country;
+                  const username   = context.username;
+
+                  const sql = 'SELECT  count(username) as ct FROM weathermap WHERE DATE("timestamp","-3 hour") = DATE("now","-3 hour") AND username = ? AND city = ?';
+                  const params = [username, city];
+                  const result = await db.get(sql, params, (err, row) => {
+                    if (err) {
+                      throw err;
+                    }
+                  });
+
+                  if (result.ct == 0) {
+                    await db.run("INSERT INTO weathermap (video_time, username, city, lat, long, temp, temp_feelslike)  VALUES(?,?,?,?,?,?,?)", [await getStreamTime(obs), context.username, city, lat, lon, temp, feels_like]);
+                  }
+
                   client.say(
                       target,
-                      `${response.name} temos ${response.main.temp}ºC com sensação térmica de ${response.main.feels_like}ºC. ${response.weather[0].description}`,
+                      `${city}(${country}) temos ${temp}ºC com sensação térmica de ${feels_like}ºC. ${temp_descr}`,
                   );
                 } catch (error) {
                   client.say(target, 'Não consegui achar sua cidade :/');
@@ -41,3 +81,9 @@ exports.default = (client, obs, mqtt, messages, botDB, commandQueue, ttsQueue) =
     });
 };
 
+
+process.on('SIGINT', function() {
+  console.log("Caught interrupt signal");
+  db.close();
+  process.exit();
+});
