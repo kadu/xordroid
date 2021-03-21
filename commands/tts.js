6d54f78a338386478c609c17f6d8cc59d0f1017a
kadu <kadu@kadu.com.br>
2021-03-21 12:08:40 -0300

    Simplified tts add

--- commands/tts.js
+++ commands/tts.js
@@ -8,48 +8,29 @@ exports.default = (client, obs, mqtt, messages, commandQueue, ttsQueue) => {
         if (isBot) return;
 
         const isTTSEnabled = TTS_ENABLED == "true";
-        const ttsCommands = [
-          '!tts'
-          ,'!entts'
-          ,'!pttts'
-          ,'!frtts'
-          ,'!rutts'
+
+        let ttsCommands=[];
+
+        const TTSLanguage = [
+          {"command": "!tts"  , "language": "pt-BR", "saidText": "disse" },
+          {"command": "!entts", "language": "en"   , "saidText": "said" },
+          {"command": "!pttts", "language": "pt-PT", "saidText": "disse" },
+          {"command": "!frtts", "language": "fr-FR", "saidText": "mentionné" },
+          {"command": "!rutts", "language": "ru-RU", "saidText": "сказал" }
         ];
+
         let parsedMessage = message.split(" ");
 
-        if(!ttsCommands.includes(parsedMessage[0])) {
-          console.log('não é tts');
-          return;
-        }
+        TTSLanguage.forEach((command) => {
+          if(parsedMessage[0] === command.command) {
+            if(!isTTSEnabled) {
+              client.say(target, "TTS is disabled | TTS está desativado");
+              return;
+            }
 
-        if(!isTTSEnabled) {
-          client.say(target, "TTS is disabled | TTS está desativado");
-          return;
-        }
-
-        if(parsedMessage[0] === '!tts' && isTTSEnabled) {
-          let fullMessage = context["display-name"] + " disse: " + message.replace("!tts ","");
-          ttsQueue.push( {'msg': fullMessage, 'lang': 'pt-BR','inputType': 'text'});
-        }
-
-        if(parsedMessage[0] === '!entts' && isTTSEnabled) {
-          let fullMessage = context["display-name"] + " says: " + message.replace("!entts ","");
-          ttsQueue.push( {'msg': fullMessage, 'lang': 'en','inputType': 'text'});
-        }
-
-        if(parsedMessage[0] === '!pttts' && isTTSEnabled) {
-          let fullMessage = context["display-name"] + " disse: " + message.replace("!pttts ","");
-          ttsQueue.push( {'msg': fullMessage, 'lang': 'pt-PT','inputType': 'text'});
-        }
-
-        if(parsedMessage[0] === '!frtts' && isTTSEnabled) {
-          let fullMessage = context["display-name"] + " disse: " + message.replace("!frtts ","");
-          ttsQueue.push( {'msg': fullMessage, 'lang': 'fr-FR','inputType': 'text'});
-        }
-
-        if(parsedMessage[0] === '!rutts' && isTTSEnabled) {
-          let fullMessage = context["display-name"] + " disse: " + message.replace("!rutts ","");
-          ttsQueue.push( {'msg': fullMessage, 'lang': 'ru-RU','inputType': 'text'});
-        }
+            let fullMessage = context["display-name"] + command.saidText  + " : " + message.replace(command.command,"");
+            ttsQueue.push( {'msg': fullMessage, 'lang': command.language,'inputType': 'text'});
+          }
+        });
     });
 };
