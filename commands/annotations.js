aecd1c8a11cf9bc76a8faa9d6c441319d2f97bfc
kadu <kadu@kadu.com.br>
2021-03-21 22:41:50 -0300

    Added annotation functionality to Xordroid

---
+++ commands/annotations.js
@@ -0,0 +1,45 @@
+const sqlite3 = require('sqlite3').verbose();
+const sqlite = require('sqlite');
+var db = null;
+
+async function getStreamTime(obs) {
+  return obs.send('GetStreamingStatus', {})
+    .then( (value) => {
+      return value.streamTimecode;
+    })
+    .catch( err => {
+      return 0;
+    });
+}
+
+async function createDB() {
+  try {
+    db = await sqlite.open({ filename: './databases/xordroid.db', driver: sqlite3.Database });
+    await db.run(`CREATE TABLE IF NOT EXISTS annotation ( id INTEGER PRIMARY KEY AUTOINCREMENT, timestamp DATETIME DEFAULT CURRENT_TIMESTAMP, video_time TEXT, username TEXT, annotation TEXT)`);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+createDB();
+
+exports.default = (client, obs, mqtt, messages, commandQueue, ttsQueue, send) => {
+    client.on('message', async (target, context, message, isBot) => {
+        if (isBot) return;
+
+        const parsedMessage = message.split(" ");
+
+        if(parsedMessage[0] === '!anota') {
+          const fullMessage = message.replace("!anota ",""); //.normalize('NFD').replace(/[\u0300-\u036f]/g, "");
+          client.say(target, `@${context.username}, anotado, valeu!`);
+          await db.run("INSERT INTO annotation (video_time, username, annotation)  VALUES(?,?, ?)", [await getStreamTime(obs), context.username, fullMessage]);
+        }
+    });
+};
+
+
+process.on('SIGINT', function() {
+  console.log("Caught interrupt signal");
+  db.close();
+  process.exit();
+});
