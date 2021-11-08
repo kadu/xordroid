a113171e12728f2495169a601fe671302215c813
kadu <kadu@kadu.com.br>
2021-11-07 22:22:03 -0300

    Removi teste de AD e atualizei data do café

--- commands/cafemaker.js
+++ commands/cafemaker.js
@@ -3,67 +3,29 @@ const dotenv = require('dotenv');
 const chalk = require('chalk');
 
 dotenv.config();
-OAUTH_TOKEN = process.env.OAUTH_TOKEN;
-
-// async function RodarAd(ad_time_sec, ad_interval_min) {
-
-//   setInterval(async () => {
-//     let options = {
-//       headers: {
-//         'Authorization': `Bearer ${OAUTH_TOKEN}`,
-//         'Client-Id': `${auth.getAuth('appID')}`, << == descobrir o q é
-//         'Content-Type': 'application/json'
-//       }
-//     };
-
-//     await console.log("tentando rodar comercial...");
-//     //recebe o broadcaster ID via GET
-//     response = await axios.get(`https://api.twitch.tv/helix/users?`, options);
-//     let broadcasterID = response.data.data[0].id;
-
-//     //solicita geração do commercial via POST
-//     await axios.post('https://api.twitch.tv/helix/channels/commercial', {
-//       broadcaster_id: broadcasterID, length: ad_time_sec
-//     }, options)
-//       .then(function (response) {
-//         console.log(response.data.data[0].message);
-//       })
-//       .catch(function (error) {
-//         console.log(error.response.data.message);
-//       });
-
-//   }, ad_interval_min * 60000);
-// }
 
 exports.default = (client, obs, mqtt, messages) => {
   client.on('message', (target, context, message, isBot) => {
 
-    let proxima_data = "03/07/2021 - Vamos conversar sobre Robotica Educacional com @JeyLAB_Robotica e o Prof. Orivaldo Santana, papo vai ser muito bacana, já coloca a Alexa pra dispertar ;)";
+    let proxima_data = "04/12/2021 - Um ano de Café Maker! bora comemorar conosco";
 
     switch (message) {
-        // case '!cafemaker':
-        // case '!cafe':
-        //     client.say(
-        //         target,
-        //         `O Próximo Café Maker será dia ${proxima_data} as 10:00 am - Playlist no youtube  https://bit.ly/ytcafemaker`,
-        //     );
+        case '!cafemaker':
+        case '!cafe':
+          client.say(
+              target,
+              `O Próximo Café Maker será dia ${proxima_data} as 16:00 - Veja os outros episódios no youtube  https://bit.ly/ytcafemaker`,
+          );
 
-        //     setTimeout(() => {
-        //       client.say(
-        //         target,
-        //         `Aproveitando, tem um projeto? quer apresentar no café? prencha o formulário -> https://forms.gle/mb3TNDmu9wtZzsTe9`,
-        //       );
-        //     }, 2000);
-        //     break;
-        case '!ad':
-          //https://github.com/hvilela0/DesktopJS/blob/master/modulos/ad.js
-          client.commercial("kaduzius",60).then((data) => {
-            console.log(chalk.redBright("***** COMERCIAL ****"));
-            console.log(data);
-          });
-          break;
+          setTimeout(() => {
+            client.say(
+              target,
+              `Aproveitando, tem um projeto? quer apresentar no café? prencha o formulário -> https://forms.gle/mb3TNDmu9wtZzsTe9`,
+            );
+          }, 2000);
+          break;        
         default:
-            break;
+          break;
     }
   });
 };
