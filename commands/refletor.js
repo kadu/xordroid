0ebf7cf503e9e7ce3d1e9ad850801477796260f1
kadu <kadu@kadu.com.br>
2021-11-08 10:44:59 -0300

    Log Color

--- commands/refletor.js
+++ commands/refletor.js
@@ -1,3 +1,6 @@
+const logs  = require('./commons/log');
+const chalk = require('chalk');
+
 exports.default = (client, obs, mqtt, messages, commandQueue, ttsQueue, send) => {
     client.on('message', (target, context, message, isBot) => {
         if (isBot) return;
@@ -12,18 +15,23 @@ exports.default = (client, obs, mqtt, messages, commandQueue, ttsQueue, send) =>
                 switch (parsedMessage[1]) {
                   case "liga":
                     mqtt.publish("homie/ircontrole/InfraRed/code/set", "0xF7C03F");
+                    logs.logs('Refletor ', 'Liga ' + String.fromCodePoint('0x1F4A1') , context.username);
                     break;
                   case "desliga":
                     mqtt.publish("homie/ircontrole/InfraRed/code/set", "0xF740BF");
+                    logs.logs('Refletor ', 'Desliga ' + String.fromCodePoint('0x1F4A1') , context.username);
                     break;
                   case "vermelho":
                     mqtt.publish("homie/ircontrole/InfraRed/code/set", "0xF720DF");
+                    logs.logs('Refletor ', String.fromCodePoint('0x1F4A1') + chalk.red.inverse('Vermelho'), context.username);
                     break;
                   case "azul":
                     mqtt.publish("homie/ircontrole/InfraRed/code/set", "0xF7609F");
+                    logs.logs('Refletor ', String.fromCodePoint('0x1F4A1') + chalk.blue.inverse('Azul'), context.username);
                     break;
                   case "verde":
                     mqtt.publish("homie/ircontrole/InfraRed/code/set", "0xF7A05F");
+                    logs.logs('Refletor ', String.fromCodePoint('0x1F4A1') + chalk.green.inverse('Verde'), context.username);
                     break;
                   default:
                     break;
