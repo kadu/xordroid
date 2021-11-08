e63b928a6400f9409bb6dfdbdbd4c375c3daaca0
kadu <kadu@kadu.com.br>
2021-11-08 10:16:35 -0300

    Log colorido

--- commands/matrix.js
+++ commands/matrix.js
@@ -1,4 +1,5 @@
 const CP_Matrix = '1c880a37-8dc6-487d-97c6-8f05963c5c75';
+const logs = require('./commons/log.js');
 
 exports.default = (client, obs, mqtt, messages) => {
   client.on('message', (target, context, message, isBot) => {
@@ -11,10 +12,12 @@ exports.default = (client, obs, mqtt, messages) => {
     switch (parsedMessage[0]) {
       case '!matrix':
         messages.push(fullMessage);
+        logs.logs('led matrix', fullMessage, context.username);
         break;
         case '!fixmatrix':
           mqtt.publish("homie/ledmatrix/message/state", "Idle");
           mqtt.publish("homie/ledmatrix/message/fixmessage/set", fullMessage);
+          logs.logs('led matrix', fullMessage, context.username);
           break;
       default:
         break;
@@ -25,6 +28,7 @@ exports.default = (client, obs, mqtt, messages) => {
     if(message.tags['custom-reward-id'] === CP_Matrix) {
       const fullMessage = message.params[1].normalize('NFD').replace(/[\u0300-\u036f]/g, "");
       messages.push(fullMessage);
+      logs.logs('led matrix', fullMessage, message.tags['display-name']);
     }
   });
 
