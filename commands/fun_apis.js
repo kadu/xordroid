d00bc2647250ec8e4d1de2b9ef8a52f84a3e17c9
kadu <kadu@kadu.com.br>
2021-11-08 09:56:06 -0300

    Adicionando log colorido

--- commands/fun_apis.js
+++ commands/fun_apis.js
@@ -1,14 +1,15 @@
-const dotenv = require('dotenv');
-const { get } = require('request-promise');
-const translate = require('translate');
+const dotenv     = require('dotenv');
+const { get }    = require('request-promise');
+const translate  = require('translate');
+const logs       = require('./commons/log');
+const funapi     = "http://api.fungenerators.com/fact/random"
+const chuckAPI   = "https://api.chucknorris.io/jokes/random"
+const quotesAPI  = "https://api.fisenko.net/v1/quotes/en/random"
+const dadJokeAPI = "https://icanhazdadjoke.com/"
+
 dotenv.config();
 translate.engine = process.env.TRANSLATE_ENGINE;
 translate.key = process.env.GOOGLE_KEY;
-const funapi = "http://api.fungenerators.com/fact/random"
-const chuckAPI = "https://api.chucknorris.io/jokes/random"
-const quotesAPI = "https://api.fisenko.net/quotes"
-const dadJokeAPI = "https://icanhazdadjoke.com/"
-
 
 exports.default = (client, obs, mqtt, messages) => {
     client.on('message', async (target, context, message, isBot) => {
@@ -25,6 +26,7 @@ exports.default = (client, obs, mqtt, messages) => {
                     target,
                     `Chuck Facts!: ${translated} (En: ${response?.value})`,
                 );
+                logs.logs('FUN APIS', message, context.username);
                 break;
             case '!quote':
               response = await get(quotesAPI);
@@ -33,8 +35,9 @@ exports.default = (client, obs, mqtt, messages) => {
 
               client.say(
                   target,
-                  `Frase!: ${translated} (Autor: ${response?.author}) `,
+                  `Frase!: ${translated} (Autor(a): ${response?.author.name}) `,
               );
+              logs.logs('FUN APIS', message, context.username);
               break;
               case '!en-quote':
               case '!enquote':
@@ -44,28 +47,36 @@ exports.default = (client, obs, mqtt, messages) => {
 
                 client.say(
                     target,
-                    `Quote: ${response?.text} (Autor: ${response?.author})`,
+                    `Quote: ${response?.text} (Author: ${response?.author.name})`,
                 );
+                logs.logs('FUN APIS', message, context.username);
                 break;
 
               case '!tiozao':
                   response = await get({uri:dadJokeAPI, json: true});
                   // response = JSON.parse(response);
-                  translated = await translate(response?.joke, { from: 'en', to: 'pt' });
+                  let textHelper = response?.joke.replaceAll('.',';');
+                  translated = await translate(textHelper, { from: 'en', to: 'pt' });
+
+                  console.log(response);
+                  console.log(translated);
 
                   client.say(
                       target,
                       `Tiozao: ${translated}`,
                   );
+                  logs.logs('FUN APIS', message, context.username);
                   break;
                 case '!dadjoke':
                     response = await get({uri:dadJokeAPI, json: true});
-                    translated = await translate(response?.joke, { from: 'en', to: 'pt' });
+                    translated = await translate(response?.joke, { text: response?.joke , from: 'en', to: 'pt' });
+                    console.log(translated);
 
                     client.say(
                         target,
                         `DadJoke: ${response?.joke}`,
                     );
+                    logs.logs('FUN APIS', message, context.username);
                     break;
               default:
                 break;
