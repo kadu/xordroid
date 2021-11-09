6c1b092d1c8a4ec95c7e883b5086101d07ffb983
kadu <kadu@kadu.com.br>
2021-11-09 10:11:20 -0300

    Log colorido

--- commands/youtube_counter.js
+++ commands/youtube_counter.js
@@ -1,19 +1,27 @@
-var request = require('request-promise');
+const logs = require('./commons/log');
+
+const request = require('request-promise');
 const dotenv = require('dotenv');
 dotenv.config();
 
 
-var id = "UC3pkYuCPdMK7aEqYXcatzNQ"; //channelID
-var key = process.env.GOOGLE_KEY;
-var url = "https://www.googleapis.com/youtube/v3/channels?part=statistics&id=" + id + "&key=" + key;
+const id = "UC3pkYuCPdMK7aEqYXcatzNQ"; //channelID
+const key = process.env.GOOGLE_KEY;
+const url = "https://www.googleapis.com/youtube/v3/channels?part=statistics&id=" + id + "&key=" + key;
 
 let getCounter = async (channelUrl) => {
-  let response = await request({
-    method: 'GET',
-    url: url
-  });
-  var json = JSON.parse(response);
-  return(json.items[0].statistics.subscriberCount);
+  try {
+    let response = await request({
+      method: 'GET',
+      url: url
+    });
+    const json = JSON.parse(response);
+    let resultado;
+      resultado = json.items[0].statistics.subscriberCount
+  } catch (error) {
+    resultado = -1;
+  }
+  return(resultado);
 }
 
 exports.default = (client, obs, mqtt, messages) => {
@@ -24,6 +32,7 @@ exports.default = (client, obs, mqtt, messages) => {
             case '!ytcount':
                 contador = await getCounter();
                 messages.push(`Valeu por chegar a ${contador} inscritos no youtube \\o/`);
+                logs.logs('YT Counter', `# ${contador}`, context.username);
                 break;
             default:
                 break;
