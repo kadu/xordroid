67a1440757b7f78e8eef015bb519158ff8e761f0
kadu <kadu@kadu.com.br>
2021-11-17 10:24:10 -0300

    adicionado logs

--- commands/base.js
+++ commands/base.js
@@ -1,3 +1,6 @@
+// const chalk  = require('chalk');
+const logs = require('./commons/log');
+
 exports.default = (client, obs, mqtt, messages, commandQueue, ttsQueue, send) => {
     client.on('message', (target, context, message, isBot) => {
         if (isBot) return;
